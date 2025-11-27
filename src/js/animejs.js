ac135dfd7f72a8e53745dd7719fbd7ff4f9eb736
nurkouu <2396410@cmontmorency.qc.ca>
2025-11-27 16:43:59 -0500

    avancement
    -text clone
    -effet sonar

--- src/js/animejs.js
+++ src/js/animejs.js
@@ -1,24 +1,44 @@
 import { animate, utils } from 'animejs';
 
 animate(".dot", {
-  height: [10, 410, 10],  
+  height: [10, 410, 10],
   duration: 1300,
   loop: true,
   ease: 'easeInOutSine',
-delay: () => utils.random(2, 2000)
+  delay: () => utils.random(2, 2000)
 });
 
 animate(".dott", {
-  height: [10, 410, 10],  
+  height: [10, 410, 10],
   duration: 1000,
   loop: true,
- ease: 'outInCirc',
-delay: 400,
+  ease: 'outInCirc',
+  delay: 400,
 });
 
 animate(".dottt", {
-  height: [10, 410, 10],  
+  height: [10, 410, 10],
   duration: 800,
   loop: true,
   easing: 'linear',
 });
+
+
+
+import { createTimeline, stagger, splitText } from 'animejs';
+
+const { chars } = splitText('p', {
+  chars: {
+    wrap: 'clip',
+    clone: 'bottom'
+  },
+});
+
+createTimeline()
+  .add(chars, {
+    y: '-100%',
+    loop: true,
+    loopDelay: 300,
+    duration: 500,
+    ease: 'inOut(2)',
+  }, stagger(50, { from: 'top' }));
