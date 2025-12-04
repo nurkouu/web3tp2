93f7b141fce38f0fd848c74728c6a79453e13a6b
nurkouu <2396410@cmontmorency.qc.ca>
2025-12-03 19:46:40 -0500

    added-icon

--- src/js/maplibre.js
+++ src/js/maplibre.js
@@ -3,6 +3,7 @@ import maplibregl from "maplibre-gl";
 const carte = new maplibregl.Map({
   container: "carte",
   style: "https://tiles.openfreemap.org/styles/bright",
-  center: [-73.8462195, 45.6125882], 
-  zoom: 9 
+  center: [-73.8462195, 45.6125882],
+  zoom: 9,
+  attributionControl: false
 });
