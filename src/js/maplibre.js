b3a9fa6bbfe11128960ed70d8168ed276c42ceb7
TTylerRRichard <2496174@cmontmorency.qc.ca>
2025-12-03 20:45:35 -0500

    updated-map

--- src/js/maplibre.js
+++ src/js/maplibre.js
@@ -1,9 +1,25 @@
 import maplibregl from "maplibre-gl";
 
+const destinations = [
+  [-73.7562194, 45.4577989], // Airport Montreal
+  [-79.6319868, 43.6821737], // Airport Toronto
+  [-61.1621873, 13.2630842] // St-vincent Airport
+];
+let destinationIndex = 0;
+
 const carte = new maplibregl.Map({
   container: "carte",
-  style: "https://tiles.openfreemap.org/styles/bright",
-  center: [-73.8462195, 45.6125882],
-  zoom: 9,
-  attributionControl: false
+  style: "https://api.maptiler.com/maps/satellite/style.json?key=WJ8oUVj7jA3CUcp0bLPr",
+  center: destinations[destinationIndex],
+  zoom: 15,
+    attributionControl: false,
+});
+
+document.body.addEventListener("click", () => {
+  destinationIndex = (destinationIndex + 1) % destinations.length;
+  carte.flyTo({
+    center: destinations[destinationIndex],
+    zoom: 15,
+    duration: 3000
+  });
 });
