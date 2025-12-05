8427a9b1bb1297e2a729305ebd78f46ac900e185
nurkouu <2396410@cmontmorency.qc.ca>
2025-12-04 20:34:26 -0500

    glitch-vfx-img

--- src/js/vfx.js
+++ src/js/vfx.js
@@ -1,13 +1,26 @@
 import { VFX } from "vfx-js";
 
-const vfx = new VFX();
+const img1 = document.querySelector('#tylerImg');
 
-
-const el = document.getElementById("titleVFX");
-
-vfx.add(el, {
-    shader,
-    uniforms: {
-        scroll: () => window.scrollY / window.innerHeight,
-    }
+const vfxT = new VFX();
+vfxT.add(img1, {
+    shader: "glitch",
+    overflow: 400
 });
+
+const img2 = document.querySelector('#nurlikaImg');
+
+const vfxN = new VFX();
+vfxN.add(img2, {
+    shader: "glitch",
+    overflow: 100
+});
+
+/* POUR LE TITRE SERAPH II
+const title = document.querySelector('#seraphII');
+
+const vfxTitle = new VFX();
+vfxTitle.add(title, {
+    shader: "glitch"
+});
+*/
